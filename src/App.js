b61a75b6d20331f28570aa018769bb227cf04d8b
krishkamani <kdkamani00@gmail.com>
2022-01-15 12:21:10 +0530

    Adding header component and links to other page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes , Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
+import Header from './component/header/header.component';
 
 import './App.css';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
